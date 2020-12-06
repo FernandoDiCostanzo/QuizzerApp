8bf8a226601221c749a376955fdb5dee688bda4a
FernandoDiCostanzo <55757804+FernandoDiCostanzo@users.noreply.github.com>
2020-12-06 19:10:17 +0100

    Added BookMark

--- app/src/main/java/com/example/quizzerapp/QuestionActivity.java
+++ app/src/main/java/com/example/quizzerapp/QuestionActivity.java
@@ -1,6 +1,10 @@
 package com.example.quizzerapp;
 
 import android.animation.Animator;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Build;
@@ -17,7 +21,9 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.example.quizzerapp.helpers.Path;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.example.quizzerapp.helpers.Utility;
 import com.example.quizzerapp.models.*;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -25,17 +31,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionActivity extends AppCompatActivity {
 
+
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference rootRef = database.getReference(Path.ROOT_PATH);
-    DatabaseReference setsRef = rootRef.child(Path.SETS_PATH);
+    DatabaseReference rootRef = database.getReference(Utility.ROOT_PATH);
+    DatabaseReference setsRef = rootRef.child(Utility.SETS_PATH);
     private TextView question,noIndicator,textCategory;
-    private FloatingActionButton bookmark;
+    private FloatingActionButton bookmarkBtn;
     private LinearLayout optionsContainer;
     private Button shareBtn,nextBtn;
     private int count =0;
@@ -43,9 +54,18 @@ public class QuestionActivity extends AppCompatActivity {
     private int position = 0;
     private int score =0;
     private int questionNum;
-    private String category;
+    private String category,imageUrl;
+    private Dialog loadingDialog;
+
+    private List<QuestionModel> bookmarkList;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private Gson gson;
+
+    private int matchedQuestionPosition;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,12 +74,20 @@ public class QuestionActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_question);
         setSupportActionBar(toolbar);
 
+        sharedPreferences = getSharedPreferences(Utility.FILE_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        gson = new Gson();
+
+        getBookmarks();
+
+
         category = getIntent().getStringExtra("category");
         questionNum = getIntent().getIntExtra("sets",1);
+        imageUrl = getIntent().getStringExtra("imageUrl");
 
         question = findViewById(R.id.textView_question);
         noIndicator = findViewById(R.id.textView_number_indicator);
-        bookmark = findViewById(R.id.btn_bookmark);
+        bookmarkBtn = findViewById(R.id.btn_bookmark);
         optionsContainer = findViewById(R.id.linearLayout_options);
         shareBtn = findViewById(R.id.button_share);
         nextBtn = findViewById(R.id.button_next);
@@ -67,11 +95,25 @@ public class QuestionActivity extends AppCompatActivity {
 
         textCategory.setText(category+" "+questionNum);
 
+        bookmarkBtn.setOnClickListener(v -> {
+            YoYo.with(Techniques.Bounce).playOn(bookmarkBtn);
+            if(modelMatch()){
+                bookmarkList.remove(matchedQuestionPosition);
+                bookmarkBtn.setImageDrawable(getDrawable(R.drawable.bookmark_border));
+            }else {
+                bookmarkList.add(list.get(position));
+                bookmarkBtn.setImageDrawable(getDrawable(R.drawable.bookmark_border_selected));
+            }
+        });
+
         list = new ArrayList<>();
 
+        loadingDialog = new Dialog(this);
+        loadingDialog.setContentView(R.layout.loading);
+        loadingDialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        loadingDialog.setCancelable(false);
 
-
-
+        loadingDialog.show();
         setsRef.child(category)
                 .child("questions")
                 .orderByChild("questionNum")
@@ -81,6 +123,8 @@ public class QuestionActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot snap : snapshot.getChildren()) {
                     QuestionModel q = snap.getValue(QuestionModel.class);
+                    q.setCategory(category);
+                    q.setImageUrl(imageUrl);
                     list.add(q);
                 }
                 if(list.size()>0){
@@ -90,49 +134,66 @@ public class QuestionActivity extends AppCompatActivity {
                             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                             @Override
                             public void onClick(View v) {
-                                checkAnsw(((Button)v));
+                                checkAnsw((Button)v);
                             }
                         });
                     }
                     playAnim(question,0,list.get(position).getQuestion());
-                    nextBtn.setOnClickListener(new View.OnClickListener() {
-                        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-                        @Override
-                        public void onClick(View v) {
-                            nextBtn.setEnabled(false);
-                            nextBtn.setAlpha(0.5f);
-                            enableOption(true);
-                            position++;
-                            if(position == list.size()){
-                                //score activity
-                                return;
-                            }
-                            count = 0;
-                            playAnim(question,0,list.get(position).getQuestion());
+                    nextBtn.setOnClickListener(v -> {
+                        nextBtn.setEnabled(false);
+                        nextBtn.setAlpha(0.5f);
+                        enableOption(true);
+                        position++;
+                        if(position == list.size()){
+                            Intent intent = new Intent(QuestionActivity.this,ScoreActivity.class);
+                            intent.putExtra("score",score);
+                            intent.putExtra("total",list.size());
+                            startActivity(intent);
+                            return;
                         }
+                        count = 0;
+                        playAnim(question,0,list.get(position).getQuestion());
                     });
+
+                    shareBtn.setOnClickListener(v -> {
+                        String body = list.get(position).getQuestion()+ "\n" +
+                                list.get(position).getOptionA()+ "\n" +
+                                list.get(position).getOptionB()+ "\n" +
+                                list.get(position).getOptionC()+ "\n" +
+                                list.get(position).getOptionD();
+                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                        shareIntent.setType("plain/text");
+                        shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Quizzer challenge");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT,body);
+                        startActivity(Intent.createChooser(shareIntent,"Share via"));
+                    });
+
                 }else{
                     finish();
                     Toast.makeText(QuestionActivity.this,"no questions",Toast.LENGTH_SHORT).show();
                 }
-
+                loadingDialog.dismiss();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(QuestionActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
+                loadingDialog.dismiss();
             }
         });
 
-
-
-
-
-
     }
 
-    private void playAnim(View view, int value,String data){
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        storeBookmarks();
+    }
+
+    private void playAnim(View view, int value, String data){
         view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(350).setStartDelay(100).setInterpolator(new DecelerateInterpolator()).setListener(new Animator.AnimatorListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onAnimationStart(Animator animation) {
                 if(value == 0 && count<4){
@@ -149,6 +210,11 @@ public class QuestionActivity extends AppCompatActivity {
 
                     playAnim(optionsContainer.getChildAt(count),0,option);
                     count++;
+                }
+                if(modelMatch()){
+                    bookmarkBtn.setImageDrawable(getDrawable(R.drawable.bookmark_border_selected));
+                }else {
+                    bookmarkBtn.setImageDrawable(getDrawable(R.drawable.bookmark_border));
                 }
             }
 
@@ -201,5 +267,35 @@ public class QuestionActivity extends AppCompatActivity {
                 optionsContainer.getChildAt(i).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#DADADA")));
             }
         }
+    }
+
+    private void getBookmarks(){
+        String json = sharedPreferences.getString(Utility.KEY_NAME,null);
+        Type type = new TypeToken<List<QuestionModel>>(){}.getType();
+        bookmarkList = gson.fromJson(json,type);
+        if(bookmarkList == null){
+            bookmarkList = new ArrayList<>();
+        }
+    }
+
+    private boolean modelMatch(){
+        boolean matched = false;
+        int pos=0;
+        for (QuestionModel questionModel : bookmarkList) {
+            if(questionModel.getQuestion().equals(list.get(position).getQuestion())
+               && questionModel.getCorrectAns().equals(list.get(position).getCorrectAns())
+               && questionModel.getQuestionNum() == list.get(position).getQuestionNum()){
+                    matched = true;
+                    matchedQuestionPosition = pos;
+            }
+            pos++;
+        }
+        return matched;
+    }
+
+    private void storeBookmarks(){
+        String json = gson.toJson(bookmarkList);
+        editor.putString(Utility.KEY_NAME,json);
+        editor.commit();
     }
 }
